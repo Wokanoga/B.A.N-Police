c1ad0ec8128b4d415d067851a28af17b51fec34c
Lincoln <wokanoga@gmail.com>
2020-02-04 00:29:13 -0800

    On_Group_Join progress, Logging

--- B.A.N Police/bp.py
+++ B.A.N Police/bp.py
@@ -1,7 +1,8 @@
 import discord
 import logging
 from discord.ext import commands
-TOKEN = 'Njc0MDA1Njg2MjM3MDAzNzk3.XjiV_w.6D0VW1d92YVtMBCeY4dFWvJGya8'
+
+TOKEN = 'Njc0MDA1Njg2MjM3MDAzNzk3.Xji2Gw.z0b6wHFaDrhLnUtV7labVGtNPjA'
 client = discord.Client()
 
 logger = logging.getLogger('discord')
@@ -12,11 +13,27 @@ logger.addHandler(handler)
 
 
 @client.event
+async def on_connect():
+    print('Connected\nLogging in\n')
+
+
+@client.event
 async def on_ready():
     print('Logged in as {0.user}, ID {0.user.id}'.format(client))
+    print('Ready to B.A.N\n')
+    that_cheese = discord.utils.get(client.get_all_channels(), id=153306465367490560)
     # print(client.user.name)
     # print(client.user.id)
-    print('------')
+    # -------------------------------------------------------------
+    # my id
+    # user = discord.utils.get(client.get_all_members(), id=81423485208891392)
+    # Anders id
+    # user = discord.utils.get(client.get_all_members(), id=140901403592884224)
+    # print(f'Target user is {user}')
+    # if user is None:
+    #     return
+    # else:
+    #     await user.send("A message for you")
 
 
 @client.event
@@ -24,9 +41,26 @@ async def on_message(message):
     # on_message fires on EVERY message in every channel that the bot is in.  Including dms.
     if message.author == client.user:
         return
-    elif message.content.startswith('B.A.N'):
-        msg = 'Hello {0.author.mention}'.format(message)
+    # elif message.content.startswith('B.A.N'):
+    elif message.content == 'B.A.N Ander':
+        user = discord.utils.get(client.get_all_members(), id=81423485208891392)
+        shadow_realm = discord.utils.get(client.get_all_channels(), id=306630991018065932)
+        if user is None:
+            return
+        else:
+            print('Gotta move the user here')
+        msg = 'You\'re fucking banned. {0.author.mention}'.format(message)
         await message.channel.send(msg)
+
+
+@client.event
+async def on_group_join(channel, user):
+    print('Event ran')
+    shadow_realm = discord.utils.get(client.get_all_channels(), id=306630991018065932)
+    if channel is shadow_realm:
+        print(f'User {user}, joined a channel that was not the Shadow Realm')
+    else:
+        print(f'User {user}, was banished to the Shadow Realm')
 
 
 client.run(TOKEN)
--- B.A.N Police/notes.md
+++ B.A.N Police/notes.md
@@ -1,3 +1,5 @@
 #B.A.N Police
 
 2.3.20: This is a side project for the encouraged automation.  I plan on this to be a discord bot that when running will continuously send a selected user to the afk channel in my discord server if they are not in it.
+
+2.3.20: Lots of old guides outdated.  Among them send_message being outdated and mostly replaced by message.channel.send or user.send.  Fuckloads of events to play with.  Getting a specific user id to send private messages was very straight- forward.  In fact everything seems pretty straight-forward so far.  I hope there is a event to detect a specific user id entering specific channel ids.  Pycharm is godlike I don't know why I haven't tried this before.
